c7583e8494fb4bec94316cd5332a77f1455fd890
Aman <aman212yadav@gmail.com>
2019-04-14 16:07:47 +0530

    first django project

---
+++ MY_FIRST_DJANGO_PROJECT/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aman2\desktop\django\my_first_django_project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
