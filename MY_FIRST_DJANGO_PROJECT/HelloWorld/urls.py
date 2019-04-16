69aa20588292e5dee4e0180e18ab4ed39295c934
Aman <aman212yadav@gmail.com>
2019-04-16 18:58:51 +0530

    workingWithTemplates

--- MY_FIRST_DJANGO_PROJECT/HelloWorld/urls.py
+++ MY_FIRST_DJANGO_PROJECT/HelloWorld/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from HelloWorld import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('hello',views.hello)
 ]
--- MY_FIRST_DJANGO_PROJECT/HelloWorld/views.py
+++ MY_FIRST_DJANGO_PROJECT/HelloWorld/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response=HttpResponse("Hello world")
+    developed_by='Aman yadav'
+    friends=[
+        " Aman yadav",
+        "Divyam",
+        "Kunal",
+        "Akash",
+        "Anuj"
+    ]
+    context={
+        "developer":developed_by,
+        "friends":friends
+    }
+    response=render(request,'HelloWorld/index.html',context)
     return response
+def hello(request):
+    return render(request,'HelloWorld/hello.html');    
