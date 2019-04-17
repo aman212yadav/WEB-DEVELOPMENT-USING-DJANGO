46b07133741c7202793087de8576862530b81b2b
Aman <aman212yadav@gmail.com>
2019-04-17 15:37:56 +0530

    handling static files

--- MY_FIRST_DJANGO_PROJECT/myFirstProject/settings.py
+++ MY_FIRST_DJANGO_PROJECT/myFirstProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm1s&1(1-)t9bpxyus+c(#b1^i)$xn_+ehbe^2kpjgz(0^%09@8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'myFirstProject.urls'
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
